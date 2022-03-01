d6a7e2d4bcd51e16582feb368c74694487a95132
taytum-twiss <twisstr@g.crookcountyschools.org>
2022-03-01 08:43:50 -0800

    pushing server for deploy on heroku

--- server.js
+++ server.js
@@ -1,11 +1,12 @@
-'use strict';
+`use strict`;
 
 // require in our libraries 
 require('dotenv').config();
-import express from 'express';
-import cors from 'cors';
+const express = require('express');
+const cors = require('cors'); 
+const axios = require('axios');
 // bring in out weather dummy data
-import { find } from './data/weather.json';
+const weather = require('./data/weather.json');
 
 // app to use...
 const app = express();
@@ -20,17 +21,27 @@ app.get('/', (request, response) => {
 });
 
 // create an API endpoint of '/weather' that processes a "GET" request that contains 'lat', 'lon' and 'searchQuery' information.
-app.get('/weather', (request, response) => {
+app.get('/weather', async (request, response) => {
+    //Access the query parameters from the web client request object, to identify the exact location for which the web client is requesting weather info.
     let lat = request.query.lat;
     let lon = request.query.lon;
     let searchQuery = request.query.searchQuery;//city we searched for 
 
+    console.log("lat, lon", lat, lon)
+
     // use the .find() method the discover which the 'lat', 'lon' and 'searchQuery' information belong to. 
-    const city = find(cityObj => cityObj.city_name.toLowerCase() === searchQuery.toLowerCase());
+    //const city = find(cityObj => cityObj.city_name.toLowerCase() === searchQuery.toLowerCase());
+
+    //Update the callback for your `/weather` route so that, instead of returning data from the `weather.json` file, it makes a Axios request to the weather API with the latitude and longitude from the web client.
+    const url = `https://api.weatherbit.io/v2.0/forecast/daily?key=${process.env.WEATHER_API_KEY}&days=7&lat=${lat}&lon=${lon}`;
+    console.log(url);
+    
+    const weatherData = await axios.get(url);
+    console.log(weatherData.data.data)
 
     try {
         // using each data point from the static data of the city that the user searched, create an array of `Forecast` objects, on for each day.
-        const weatherArray = city.data.map(day => new Forecast(day));
+        const weatherArray = weatherData.data.data.map(day => new Forecast(day));
 
         // send the full array back to the client who requested data from the `weather` endpoint.
         response.status(200).send(weatherArray);
